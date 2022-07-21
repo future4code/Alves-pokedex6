5b04d7096392c5a03adb5deaa533bf87277c7672
gabrielwenchenck <wenck13@gmail.com>
2022-07-21 17:17:09 -0300

    detalhes

--- pokedex/src/Page/ListaPokemon/ListaPokemon.js
+++ pokedex/src/Page/ListaPokemon/ListaPokemon.js
@@ -1,54 +1,74 @@
 import React, { useContext, useEffect } from "react";
 import ImagemPokemon from "../../Components/ImagemPokemon";
 import TipoPokemon from "../../Components/TipoPokemon";
-import { Card, Conainer, CardMain, Titulo, PegaPokemon, BlocoBotao, Detalhe, TituloPagina } from "./ListaCss";
+import {
+  Card,
+  Conainer,
+  CardMain,
+  Titulo,
+  PegaPokemon,
+  BlocoBotao,
+  Detalhe,
+  TituloPagina,
+} from "./ListaCss";
 import { useNavigate } from "react-router-dom";
-import { pokeDex } from "../../Router/Coordinator";
+import { detalhe } from "../../Router/Coordinator";
 import { GlobalContext } from "../../Global/GlobalContext";
 
 function ListaPokemon() {
-    const navigate = useNavigate();
-    const { adiciona, setAdiciona, listaPokemon, setListaPokemon, teste } = useContext(GlobalContext);
+  const navigate = useNavigate();
+  const { adiciona, setAdiciona, listaPokemon, setListaPokemon, teste } =
+    useContext(GlobalContext);
 
-    useEffect(() => { teste() }, [])
+  useEffect(() => {
+    teste();
+  }, []);
 
-    const adicionaPokemon = (nome, url) => {
-        const item = {
-            name: nome,
-            url: url,
-        };
-        const pegaItem = [...adiciona, item];
-        setAdiciona(pegaItem);
-
-        setListaPokemon(listaPokemon.filter((item) => {
-            return item.name !== nome
-        }))
+  const adicionaPokemon = (nome, url) => {
+    const item = {
+      name: nome,
+      url: url,
     };
+    const pegaItem = [...adiciona, item];
+    setAdiciona(pegaItem);
 
-    return (
-        <div>
-            <TituloPagina>Todos Pokémons</TituloPagina>
-            <Conainer>
-                {listaPokemon.map((item, index, cor) => {
-                    return (
-                        <CardMain key={index}>
-                            <Card color={cor}>
-                                <div>
-                                    <Titulo>{item.name}</Titulo>
-                                    <TipoPokemon url={item.url} />
-                                </div>
-                                <ImagemPokemon url={item.url} />
-                            </Card>
-                            <BlocoBotao>
-                                <Detalhe href="#" alt='uiii'>Detalhe</Detalhe>
-                                <PegaPokemon onClick={() => adicionaPokemon(item.name, item.url)}>Pega Pokemon</PegaPokemon>
-                            </BlocoBotao>
-                        </CardMain>
-                    );
-                })}
-            </Conainer>
-        </div>
+    setListaPokemon(
+      listaPokemon.filter((item) => {
+        return item.name !== nome;
+      })
     );
+  };
+
+  return (
+    <div>
+      <TituloPagina>Todos Pokémons</TituloPagina>
+      <Conainer>
+        {listaPokemon.map((item, index, cor) => {
+          return (
+            <CardMain key={index}>
+              <Card color={cor}>
+                <div>
+                  <Titulo>{item.name}</Titulo>
+                  <TipoPokemon url={item.url} />
+                </div>
+                <ImagemPokemon url={item.url} />
+              </Card>
+              <BlocoBotao>
+                <Detalhe onClick={() => detalhe(navigate, item.name)}>
+                  Detalhe
+                </Detalhe>
+                <PegaPokemon
+                  onClick={() => adicionaPokemon(item.name, item.url)}
+                >
+                  Pega Pokemon
+                </PegaPokemon>
+              </BlocoBotao>
+            </CardMain>
+          );
+        })}
+      </Conainer>
+    </div>
+  );
 }
 
 export default ListaPokemon;
--- pokedex/src/Router/Coordinator.js
+++ pokedex/src/Router/Coordinator.js
@@ -1,17 +1,11 @@
 import React from "react";
 
 export const listPokemon = (navigate) => {
-
-    navigate('/')
-
-}
-export const detalhe = (navigate) => {
-
-    navigate('/detalhe')
-
-}
+  navigate("/");
+};
+export const detalhe = (navigate, name) => {
+  navigate(`/detalhe/${name}`);
+};
 export const pokeDex = (navigate) => {
-
-    navigate('pokedex')
-
-}
+  navigate("pokedex");
+};
