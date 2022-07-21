7e139eccd9d5954bd9282d06842624b772daaab9
Lucas Rafael <lucas-rafael_@hotmail.com>
2022-07-21 10:38:42 -0300

    Resolvendo alguns bugs

--- pokedex/src/Global/GlobalState.js
+++ pokedex/src/Global/GlobalState.js
@@ -1,18 +1,34 @@
 import React, { useState } from "react";
 import { GlobalContext } from './GlobalContext'
+import axios from "axios"
+import { BASE_URL } from "../Components/BaseUrl";
 
 
 export default function GlobalState(props) {
     const [adiciona, setAdiciona] = useState([])
     const [listaPokemon, setListaPokemon] = useState([])
 
+
+    const teste = () => {
+        if (listaPokemon.length > 0) {
+            return
+        } else {
+            axios.get(`${BASE_URL}pokemon/?offset=0&limit=20`)
+                .then((response) => {
+                    setListaPokemon(response.data.results)
+                }).catch((erro) => {
+                    console.log(erro.message)
+                })
+        }
+    }
+
     const values = {
         adiciona,
         setAdiciona,
         listaPokemon,
-        setListaPokemon
+        setListaPokemon,
+        teste
     }
-
 
     const Provider = GlobalContext.Provider
 
--- pokedex/src/Page/PokeDex/PokeDex.js
+++ pokedex/src/Page/PokeDex/PokeDex.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import ImagemPokemon from "../../Components/ImagemPokemon";
 import TipoPokemon from "../../Components/TipoPokemon";
 import { useNavigate } from "react-router-dom";
