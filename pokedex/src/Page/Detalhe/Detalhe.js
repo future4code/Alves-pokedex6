b4803299c911b44ab923a49eef481d712397e117
Lucas Rafael <lucas-rafael_@hotmail.com>
2022-07-22 17:12:27 -0300

    Finalizando CSS Detalhe

--- pokedex/src/Page/Detalhe/Detalhe.js
+++ pokedex/src/Page/Detalhe/Detalhe.js
@@ -1,21 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
-import { Card, CardMain, Titulo } from "../ListaPokemon/ListaCss";
-import { detalhe } from "../../Router/Coordinator";
-import styled from "styled-components";
-
-const DivImagens = styled.img`
-  border: solid 1px black;
-`;
-
-const Container = styled.div`
-  background-color: #729f92;
-`;
-
-const Border = styled.div`
-  border: solid 1px black;
-`;
+import { Container, Imagem, BlocoImagem, ConteudoTipo, Status, Fundo, FundoStatus, StatusNumero, Separacao, Progresso, Nome, TipoNome, Ataques, FundoAtaque, ImagemPokemon, TituloDetalhe } from './DetalheCss'
+import ImagemTipo from '../../Components/ImagemTipo'
 
 function Detalhe() {
   const navigate = useNavigate();
@@ -41,33 +28,63 @@ function Detalhe() {
 
   console.log(detalhes?.name);
 
-  const listaStats = detalhes.stats?.map((stats) => {
+  const listaStats = detalhes.stats?.map((stats, index) => {
     return (
-      <div>
-        <p>{stats?.base_stat}</p>
-        <p>{stats?.stat.name}</p>
+      <div key={index}>
+        <Status>{stats?.stat.name}</Status>
+        <StatusNumero>{stats?.base_stat}</StatusNumero>
+        <Progresso value={stats?.base_stat} max={100} />
+        <Separacao></Separacao>
       </div>
     );
   });
 
   const primeirosMoves = detalhes.moves?.slice(0, 4);
 
-  const listaMoves = primeirosMoves?.map((move) => {
+  const listaMoves = primeirosMoves?.map((move, index) => {
     return (
-      <div>
-        <p>{move?.move.name}</p>
+      <div key={index}>
+        <Ataques>{move?.move.name}</Ataques>
       </div>
     );
   });
 
+  const tipo = detalhes.types?.map((item, index) => {
+    console.log(item)
+    return <>
+      <dConteudoTipoiv>
+        <TipoNome color={item.type.name}> <img src={ImagemTipo(item.type.name)} alt={item.type.name} />{item.type.name}</TipoNome>
+      </dConteudoTipoiv>
+    </>
+  })
+
   return (
-    <Container>
-      <DivImagens src={detalhes.sprites?.back_default} />
-      <DivImagens src={detalhes.sprites?.front_default} />
-      <p>{detalhes?.name}</p>
-      {listaStats}
-      {listaMoves}
-    </Container>
+    <Fundo>
+      <TituloDetalhe>Detalhe</TituloDetalhe>
+      <Container>
+        <BlocoImagem>
+          <div>
+            <Imagem src={detalhes.sprites?.front_default} />
+          </div>
+          <div>
+            <Imagem src={detalhes.sprites?.back_default} />
+          </div>
+        </BlocoImagem>
+        <FundoStatus>
+          <h2>Base Status</h2>
+          <p>{listaStats}</p>
+        </FundoStatus>
+        <div>
+          <Nome>{detalhes?.name}</Nome>
+          {tipo}
+          <FundoAtaque>
+            <h1>Moves:</h1>
+            {listaMoves}
+          </FundoAtaque>
+        </div>
+        <ImagemPokemon src={detalhes.sprites?.other["official-artwork"].front_default} />
+      </Container>
+    </Fundo>
   );
 }
 
